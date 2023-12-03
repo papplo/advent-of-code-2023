91298978e5ab25bfe85b9c4342341ad594d9cda3
Pablo Anttila <pablo@queenslab.se>
2023-12-03 22:16:09 +0100

    Splittin strings like a ballerina. Should find a more adept way of destructuring values

--- day-2/day-2.go
+++ day-2/day-2.go
@@ -3,19 +3,64 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // determine possible games if
-// bag has constraints 12 red, 13 green, 15 blue
+// bag has constraints 12 red, 13 green, 14 blue
+func isGamePossible(gameData string) (string, bool) {
+	gameRules := make(map[string]int)
 
-// sum id's of game id's that are possible
+	gameRules["red"] = 12
+	gameRules["green"] = 13
+	gameRules["blue"] = 14
+
+	gameDataClean := strings.SplitAfter(gameData, "Game")
+	gameId := strings.Split(gameDataClean[1], ":")[0]
+	gameHands := strings.Split(gameDataClean[1], ":")[1]
+
+	var isPossible bool
+
+	for _, hand := range strings.Split(gameHands, ";") {
+		for _, cubes := range strings.Split(hand, ",") {
+			token := strings.Fields(cubes)
+			amount, err := strconv.Atoi(token[0])
+			if err == nil {
+
+				if amount > gameRules[token[1]] {
+					isPossible = false
+					return strings.TrimSpace(gameId), isPossible
+				}
+
+				isPossible = amount <= gameRules[token[1]]
+			} else {
+				println(err)
+			}
+		}
+	}
+
+	return strings.TrimSpace(gameId), isPossible
+}
+
 func main() {
 	file, err := os.ReadFile("input.txt")
 	if err != nil {
 		return
 	}
 
-	println(file)
+	var sum int
+	for _, gameData := range strings.Split(string(file), "\n") {
+		gameId, isPossible := isGamePossible(gameData)
+		if isPossible {
+			id, error := strconv.Atoi(gameId)
+			sum += id
+			println(id, sum)
+			if error != nil {
+				panic("whoea")
+			}
+		}
+	}
 
 	output, err := os.Create("output.txt")
 	if err != nil {
@@ -23,7 +68,7 @@ func main() {
 	}
 
 	defer output.Close()
-	output.WriteString(fmt.Sprint("results here"))
-	fmt.Printf("AoC: Day 2, part 1: %d", 000)
+	output.WriteString(fmt.Sprint(sum))
+	fmt.Printf("AoC: Day 2, part 1: %d", sum)
 
 }
