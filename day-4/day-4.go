17c96a90e641a5fe88c580f64886abda0d3929a5
Pablo Anttila <pablo@queenslab.se>
2023-12-10 14:38:34 +0100

    Part two solved for input_test, should test more for real input

--- day-4/day-4.go
+++ day-4/day-4.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -12,61 +13,91 @@ func aocInput() []string {
 	return strings.Split(string(file), "\n")
 }
 
-func aocOutput(r int) {
+func aocOutput(r []int) {
 	output, err := os.Create("output.txt")
 	if err != nil {
 		return
 	}
 
 	defer output.Close()
-	output.WriteString(fmt.Sprint(r))
+	output.WriteString(fmt.Sprintln(r))
 }
 
-func scratchCards(lines []string, index int) (int, int) {
-	r, _ := regexp.Compile("([0-9]){1,3}")
-	nums := r.FindAllString(lines[index], -1)
+type Round struct {
+	game    int
+	matches int
+	points  int
+	copies  int
+}
 
-	var matchesInGame int
-	// gameNumber := nums[0:1]
-	winningNumbers := nums[1:11]
-	myNumbers := nums[11:]
+func reduce[T, M any](s []T, f func(M, T) M, initValue M) M {
+	acc := initValue
+	for _, v := range s {
+		acc = f(acc, v)
+	}
+	return acc
+}
+
+func scratchCards(lines *[]string, index int) Round {
+	r, _ := regexp.Compile("([0-9]){1,3}")
+	line := (*lines)[index]
+	nums := r.FindAllString(line, -1)
+
+	gameNumber, _ := strconv.Atoi(nums[0:1][0])
+	round := Round{game: gameNumber - 1, copies: 1}
+	winningNumbers := nums[1:6]
+	myNumbers := nums[6:]
 
 	for _, w := range winningNumbers {
 		for _, m := range myNumbers {
 			if m == w {
-				matchesInGame += 1
+				round.matches++
 			}
 		}
 	}
 
-	scratchedCards := 1
-
-	gamePoints := min(matchesInGame, 1)
-	for j := 1; j < matchesInGame; j++ {
-		gamePoints = gamePoints * 2
-		_, recursiveScrathes := scratchCards(lines, j)
-
-		scratchedCards = +recursiveScrathes
+	round.points = min(round.matches, 1)
+	for j := 1; j < round.matches; j++ {
+		round.points = round.points * 2
 	}
 
 	// if gamePoints > 0 {
 	// 	fmt.Printf("Matches in game %v: %v, POINTS: %v\n", gameNumber, matchesInGame, gamePoints)
 	// }
-	return gamePoints, scratchedCards
+	return round
 }
 
 func main() {
 	input := aocInput()
 
-	var totalPoints int
-	var totalCards int
+	cards := make([]Round, 0, len(input))
+	var total int
+	var points int
 
 	for i := 0; i < len(input); i++ {
-		gamePoints, scratchedCards := scratchCards(input, i)
-
-		totalCards += scratchedCards
-		totalPoints += gamePoints
+		r := scratchCards(&input, i)
+		cards = append(cards, r)
+		points += r.points
 	}
 
-	aocOutput(totalCards)
+	// for _, round := range cards {
+	// 	fmt.Printf("Game %v: matches(%v), copies(%v)\n", round.game, round.matches, round.copies)
+	// }
+
+	total = reduce(cards,
+		func(acc int, current Round) int {
+			fmt.Printf("Game %v: matches(%v), copies(%v)\n", current.game, current.matches, current.copies)
+
+			// matches for this card
+			for i := current.matches; i >= 0; i-- {
+				cards[current.game+i].copies += current.copies
+			}
+
+			acc = acc + current.matches*current.copies
+			println(acc)
+			return acc
+		}, len(cards))
+
+	aocOutput(append(make([]int, 0), points, total))
+	fmt.Printf("Total: %v scratchcards \n", total)
 }
