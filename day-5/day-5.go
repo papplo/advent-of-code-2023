011e5f728d896c0990308eb71ad8cac37ca474a3
Pablo Anttila <pablo@queenslab.se>
2023-12-10 23:27:26 +0100

    Split input with regex

--- day-5/day-5.go
+++ day-5/day-5.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
 func aocInput() []string {
-	file, _ := os.ReadFile("input.txt")
-	return strings.Split(string(file), "\n")
+	file, _ := os.ReadFile("input_test.txt")
+	return strings.Split(string(file), "\n\n")
 }
 
 func aocOutput(r []int) {
@@ -21,6 +22,28 @@ func aocOutput(r []int) {
 	output.WriteString(fmt.Sprintln(r))
 }
 
-func main() {
+var mapper = map[string][]int{}
 
+func findSequences(in string) (string, []string) {
+	rname, _ := regexp.Compile("(.+(-to-).[^\t\n\f\r ]+)")
+	r, _ := regexp.Compile("([0-9]).+[^\t\n\f\r ]")
+
+	name := rname.FindString(in)
+	seq := r.FindAllString(in, -1)
+
+	return name, seq
+}
+
+func main() {
+	input := aocInput()
+
+	// sequences
+	// _, seeds := findSequences(input[0])
+	// var bigmap map[string][][]int
+
+	for _, s := range input[1:] {
+		maps, seq := findSequences(s)
+		fmt.Println(maps, seq)
+
+	}
 }
